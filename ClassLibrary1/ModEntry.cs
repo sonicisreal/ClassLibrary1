62c6ed24176f3e658d8ee77027834fdf3de8b906
sonicisreal <168301101+sonicisreal@users.noreply.github.com>
2025-08-15 08:59:07 -0400

    Add project files.

---
+++ ClassLibrary1/ModEntry.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.Xna.Framework;  
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using System.Collections.Generic;
+
+namespace MyTimePauseMod
+{
+    public class ModEntry : Mod
+    {
+        private bool isTimePaused = false;
+
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
+        }
+
+        private void OnUpdateTicked(object sender, UpdateTickedEventArgs e)
+        {
+            if (!Context.IsWorldReady || !Context.IsMultiplayer)
+                return;
+            if (!Context.IsMultiplayer)
+            {
+                this.Monitor.Log($"{e}", LogLevel.Debug);
+                return;
+            }
+
+            bool shouldPause = false;
+            foreach (Farmer player in Game1.getOnlineFarmers())
+            {
+                if (ShouldPauseForPlayer(player))
+                {
+                    shouldPause = true;
+                    break;
+                }
+            }
+
+            if (Context.IsMainPlayer && shouldPause != isTimePaused)
+            {
+                isTimePaused = shouldPause;
+                UpdateTimePause(shouldPause);
+            }
+        }
+
+        private bool ShouldPauseForPlayer(Farmer player)
+        {
+            return player.CurrentTool != null && player.UsingTool ||
+                   Game1.activeClickableMenu != null && player.Equals(Game1.player) ||
+                   player.isInBed.Value ||
+                   Game1.eventUp ||
+                   Game1.currentMinigame != null ||
+                   Game1.isFestival() ||
+                   Game1.farmEvent != null ||
+                   player.isEmoting ||
+                   player.isEating ||
+                   Game1.paused;
+        }
+
+
+        
+
+        private void UpdateTimePause(bool shouldPause)
+        {
+            if (shouldPause)
+            {
+                Game1.gameTimeInterval = 0; 
+            }
+            else
+            {
+                Game1.gameTimeInterval = 7000;
+            }
+        }
+    }
+
+    public class PauseMessage
+    {
+        public bool ShouldPause { get; set; }
+    }
+}
